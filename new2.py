164512da026c7041a41d440f62dd8d0b5a023b09
Данилин Владислав Игоревич <dvi@L-PACK.local>
2020-03-27 15:12:57 +0300

    new2 - работает

--- new2.py
+++ new2.py
@@ -1,38 +1,31 @@
 
 
 import serial
-import serial.threaded
 import sys
 
-ser = serial.Serial()
-ser.port = "com6" #argv[1] if len(argv) > 1 else "/dev/ttyACM0"
-ser.timeout = 1
+ser             = serial.Serial()
+ser.port        = "com7"
+ser.baudrate    =  9600
+ser.timeout     = 1
 
 # try to connect
 try:
     ser.open()
 except serial.SerialException as e:
-    print("Error, could not connect to the boards.",e)
+    print("Error:",e)
     serial = None
     exit(1)
 
 
-# def Open():
-#     global serial
-#     if len(argv) > 1 and argv[1] in ("-h", "--help"):
-#         print(
-#             "Usage: %s [serial device]\n\tDefault device is /dev/ttyACM0\n" % (argv[0]))
-#         exit(0)
+while True:
+    print('.')
+  
+    resp = ser.readline()
+  
+    # Вариант вывода 1
+    if len(resp) > 0:
+        print(resp)
 
-#     serial = Serial()
-#     serial.port = argv[1] if len(argv) > 1 else "/dev/ttyACM0"
-#     serial.timeout = CONNECTION_TIMEOUT
 
-#     # try to connect
-#     try:
-#         serial.open()
-#     except SerialException, e:
-#         print("Error, could not connect to the boards.")
-#         stderr.write("%s\n" % (e))
-#         serial = None
-#         exit(1)
+print('exit - ok')
+
