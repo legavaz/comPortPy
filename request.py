6626b7345e1d43eea194fc1df92251ac10edf0bd
Данилин Владислав Игоревич <dvi@L-PACK.local>
2020-04-15 17:03:21 +0300

    начало интеграции в 1с (расширение и обработка)

--- request.py
+++ request.py
@@ -1,15 +1,46 @@
-import requests 
-url     =   'http://1c-www-server/Buh-L-PACK/hs/numenkl/nom/'
-param   =   '1872,7472'
-user    =   '1cv8'
-password=   ''
-req     = requests.get(url+param, auth=(user,password))  
+import requests
+import sqlite3
+import json
 
-print(req.url)
+# Параметры подключения к 1С
+url = 'http://localhost/danv_copy_lpack_buh3/hs/staffstatis/import/'
+param = '1872_python'
+user = '1cv8'
+password = ''
 
-if req.status_code==200:
+
+# Параметры подключения к временной базе
+conn = sqlite3.connect("checkStaff.db")  # или :memory: чтобы сохранить в RAM
+cursor = conn.cursor()
+
+
+def cleanStr(strIn):
+    goodLetter = '0123456789.'
+    result = ''
+    for let in strIn:
+        for blet in goodLetter:
+            if let == blet:
+                result += let
+
+    return result
+
+
+sql = "SELECT * FROM 'checkStaff'"
+cursor.execute(sql)
+result = cursor.fetchall()
+mList = []
+
+for target_list in result:    
+    param = {'propusk': cleanStr(target_list[0]),
+             'time': target_list[1],
+             'timeInt': target_list[2]}
+    mList.append(param)
+
+param = json.dumps(mList)
+# print(param)
+req = requests.get(url+param, auth=(user, password))
+if req.status_code == 200:
     print(req.text)
 else:
-    print('ошибка подключения',req.status_code)
-
+    print('ошибка подключения', req.status_code)
 
